019d6fa2371ce5518c5c92ccd759ffedabda95e8
Paulinho <99299721+paulinhodeveloper@users.noreply.github.com>
2022-05-18 19:34:49 -0300

    Update checkpoint.js

--- Programacao Imperativa/Aula09_Checkpoint_18_05_2022/checkpoint.js
+++ Programacao Imperativa/Aula09_Checkpoint_18_05_2022/checkpoint.js
@@ -1,31 +1,31 @@
 const pratos = {
-    1: { nome: 'Pipoca', time: 10 },
-    2: { nome: 'Macarrão', time: 8 },
-    3: { nome: 'Carne', time: 15 },
-    4: { nome: 'Feijão', time: 12 },
-    5: { nome: 'Brigadeiro', time: 8 },
-  };
-  
-  function fazerPrato(numeroPrato, time) {
-    const prato = pratos[numeroPrato];
-  
-    if (!pratos) {
-      return console.log('Prato inexistente');
-    }
-  
-    let visor;
-    const timeDefault = prato.time;
-  
-    if (time < timeDefault) {
-      visor = 'Tempo insuficiente';
-    } else if (time > timeDefault * 2 && time <= timeDefault * 3) {
-      visor = 'A comida queimou';
-    } else if (time > timeDefault * 3) {
-      visor = 'Kabummm';
-    } else {
-      visor = 'Prato pronto, bom apeite!!!';
-    }
-  
-   return console.log(visor);
+  1: { nome: 'Pipoca', time: 10 },
+  2: { nome: 'Macarrão', time: 8 },
+  3: { nome: 'Carne', time: 15 },
+  4: { nome: 'Feijão', time: 12 },
+  5: { nome: 'Brigadeiro', time: 8 },
+};
+
+function fazerPrato(numeroPrato, time) {
+  const prato = pratos[numeroPrato];
+
+  if (!pratos) {
+    return console.log('Prato inexistente Prato pronto, bom apeite!!!');
   }
-console.log(fazerPrato(2,8))
+
+  let visor;
+  const timeDefault = prato.time;
+
+  if (time < timeDefault) {
+    visor = 'Tempo insuficiente Prato pronto, bom apeite!!!';
+  } else if (time > timeDefault * 2 && time <= timeDefault * 3) {
+    visor = 'A comida queimou Prato pronto, bom apeite!!!';
+  } else if (time > timeDefault * 3) {
+    visor = 'Kabummm Prato pronto, bom apeite!!!';
+  } else {
+    visor = 'Prato pronto, bom apeite!!!';
+  }
+
+ return console.log(visor);
+}
+console.log(fazerPrato(4,10))
