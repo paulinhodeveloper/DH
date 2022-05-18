e0e90a2d4fd930815a20b60621e0f22e8f66b2a5
Paulo H S Borges <paulinhodeveloper@outlook.com>
2022-05-18 18:49:56 -0300

    checkpoint

---
+++ Programacao Imperativa/Aula4_09_05_2022/aula4.js
@@ -0,0 +1,9 @@
+const Nome = "Ana";
+let Idade;
+let Peso;
+let Altura;
+Plano = false;
+let IMC;
+
+IMC = this.peso / (this.altura * this.altura);
+console.log("")
---
+++ Programacao Imperativa/Aula5_10_05_2022/aula5.js
@@ -0,0 +1,5 @@
+function IdadeCachorro(idadeNormal = 1, idadeCachorro = 7){
+    return idade = idadeNormal * idadeCachorro;
+}
+
+console.log(IdadeCachorro(idade));
