f577627841db6a0c27226195826f6820f013464a
Paulinho <99299721+paulinhodeveloper@users.noreply.github.com>
2022-05-18 19:55:35 -0300

    Update checkpoint.js

--- Programacao Imperativa/Aula09_Checkpoint_18_05_2022/checkpoint.js
+++ Programacao Imperativa/Aula09_Checkpoint_18_05_2022/checkpoint.js
@@ -6,26 +6,33 @@ const pratos = {
   5: { nome: 'Brigadeiro', time: 8 },
 };
 
+//função fazer pratos
 function fazerPrato(numeroPrato, time) {
   const prato = pratos[numeroPrato];
 
+  //validação de valor inexistente
   if (!pratos) {
-    return console.log('Prato inexistente');
+    return console.log('Prato inexistente. Prato pronto, bom apeite!!!');
   }
 
+  //visor do micro-ondas
   let visor;
   const timeDefault = prato.time;
 
+  //logica do micro-ondas
   if (time < timeDefault) {
-    visor = 'Tempo insuficiente';
+    visor = 'Tempo insuficiente. Prato pronto, bom apeite!!!';
   } else if (time > timeDefault * 2 && time <= timeDefault * 3) {
-    visor = 'A comida queimou';
+    visor = 'A comida queimou. Prato pronto, bom apeite!!!';
   } else if (time > timeDefault * 3) {
-    visor = 'Kabummm';
+    visor = 'Kabummm.  Prato pronto, bom apeite!!!';
   } else {
     visor = 'Prato pronto, bom apeite!!!';
   }
 
+//armazena resultado do micro-ondas
  return console.log(visor);
 }
-console.log(fazerPrato(4,10) + Prato pronto, bom apeite!!!)
+
+//intereção com usuário (Prato + Comida)
+console.log(fazerPrato(4,20))
