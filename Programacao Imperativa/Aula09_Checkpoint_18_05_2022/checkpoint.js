2f0e7749d50aa08a76bccffe3453bdd23694667d
Paulo H S Borges <paulinhodeveloper@outlook.com>
2022-05-22 19:05:36 -0300

    checkpoint_part2

--- Programacao Imperativa/Aula09_Checkpoint_18_05_2022/checkpoint.js
+++ Programacao Imperativa/Aula09_Checkpoint_18_05_2022/checkpoint.js
@@ -1,31 +1,44 @@
+//armazenamento das constantes e tempos default
+
 const pratos = {
-    1: { nome: 'Pipoca', time: 10 },
-    2: { nome: 'Macarrão', time: 8 },
-    3: { nome: 'Carne', time: 15 },
-    4: { nome: 'Feijão', time: 12 },
-    5: { nome: 'Brigadeiro', time: 8 },
-  };
-  
-  function fazerPrato(numeroPrato, time) {
-    const prato = pratos[numeroPrato];
-  
-    if (!pratos) {
-      return console.log('Prato inexistente');
-    }
-  
-    let visor;
-    const tempoDefault = prato.time;
-  
-    if (time < tempoDefault) {
-      visor = 'Tempo insuficiente';
-    } else if (time > tempoDefault * 2 && time <= tempoDefault * 3) {
-      visor = 'A comida queimou';
-    } else if (time > tempoDefault * 3) {
-      visor = 'Kabummm';
-    } else {
-      visor = 'Prato pronto, bom apeite!!!';
-    }
-  
-   return console.log(visor);
+  1: { nome: 'Pipoca', time: 10 },
+  2: { nome: 'Macarrão', time: 8 },
+  3: { nome: 'Carne', time: 15 },
+  4: { nome: 'Feijão', time: 12 },
+  5: { nome: 'Brigadeiro', time: 8 },
+};
+
+//função fazer pratos
+function fazerPrato(numeroPrato, time) {
+
+  //armazena valor precionado no micro-ondas
+  const prato = pratos[numeroPrato];
+
+  //validação de valor inexistente
+  if (!pratos) {
+    return console.log('Prato inexistente. Prato pronto, bom apeite!!!');
   }
-console.log(fazerPrato(2,8))
+
+  //visor do micro-ondas
+  let visor;
+
+  //armazenamento do tempos
+  const timeDefault = prato.time;
+
+  //logica do micro-ondas
+  if (time < timeDefault) {
+    visor = 'Tempo insuficiente. Prato pronto, bom apeite!!!';
+  } else if (time > timeDefault * 2 && time <= timeDefault * 3) {
+    visor = 'A comida queimou. Prato pronto, bom apeite!!!';
+  } else if (time > timeDefault * 3) {
+    visor = 'Kabummm.  Prato pronto, bom apeite!!!';
+  } else {
+    visor = 'Prato pronto, bom apeite!!!';
+  }
+
+//armazena resultado do micro-ondas
+ return console.log(visor);
+}
+
+//intereção com usuário (Prato + tempo)
+console.log(fazerPrato(4,20))
