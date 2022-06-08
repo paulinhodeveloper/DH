afb5f7bd815ce53392efcc8cdbff5955bba0691d
Paulo H S Borges <paulinhodeveloper@outlook.com>
2022-06-08 20:56:25 -0300

    aula18-primeiras_resolucoes

---
+++ Programacao Imperativa/Aula18_08-06-2022/aula18.js
@@ -0,0 +1,19 @@
+ loopDePares = () => {
+     for (let i = 1; i <= 100; i++) {
+         if (i % 2 == 0) {
+             console.log(' O número ' + i + ' é par ');
+         }
+     }
+ }
+ console.log(loopDePares());
+
+
+loopDeImpares = () => {
+     for (let i = 1; i <= 100; i++) {
+         if (i % 2 == 1) {
+             console.log(' O número ' + i + ' é impar ');
+         }
+     }
+ }
+ console.log(loopDeImpares());
+
