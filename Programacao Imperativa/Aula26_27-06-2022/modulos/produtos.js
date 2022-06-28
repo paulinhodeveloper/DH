58e3154eb086823aea73e73b99684adc34ca3e36
Paulo H S Borges <paulinhodeveloper@outlook.com>
2022-06-28 20:15:41 -0300

    Aula26_28-06-2022

--- Programacao Imperativa/Aula26_27-06-2022/modulos/produtos.js
+++ Programacao Imperativa/Aula26_27-06-2022/modulos/produtos.js
@@ -1,50 +1,50 @@
 let produtos = [
     {
-        "nome": "licença Microsoft Windows 10", 
-        "preco": 500, 
-        "qualidade": 8, 
+        "nome": "licença Microsoft Windows 10",
+        "preco": 500,
+        "qualidade": 8,
         "status": true
     },
     {
-        "nome": "licença Microsoft Windows 11", 
+        "nome": "licença Microsoft Windows 11",
         "preco": 700,
-        "qualidade": 10, 
+        "qualidade": 10,
         "status": false
     },
     {
         "nome": "licença Microsoft Office365",
-        "preco": 1200, 
-        "qualidade": 9, 
+        "preco": 1200,
+        "qualidade": 9,
         "status": true
     },
     {
-        "nome": "licença Microsoft Office 2016", 
-        "preco": 250, 
-        "qualidade": 8, 
+        "nome": "licença Microsoft Office 2016",
+        "preco": 250,
+        "qualidade": 8,
         "status": true
     },
     {
-        "nome": "licença Microsoft Office 2013", 
-        "preco": 100, 
-        "qualidade": 5, 
+        "nome": "licença Microsoft Office 2013",
+        "preco": 100,
+        "qualidade": 5,
         "status": true
     },
     {
-        "nome": "licença Microsoft OneDrive 1TB", 
-        "preco": 250, 
-        "qualidade": 3, 
+        "nome": "licença Microsoft OneDrive 1TB",
+        "preco": 250,
+        "qualidade": 3,
         "status": false
     },
     {
-        "nome": "licença Microsoft OneDrive 2TB", 
-        "preco": 499, 
-        "qualidade": 6, 
+        "nome": "licença Microsoft OneDrive 2TB",
+        "preco": 499,
+        "qualidade": 6,
         "status": true
     },
     {
-        "nome": "licença Microsoft OneDrive Family", 
-        "preco": 700, 
-        "qualidade": 5, 
+        "nome": "licença Microsoft OneDrive Family",
+        "preco": 700,
+        "qualidade": 5,
         "status": true
     }
 ];
